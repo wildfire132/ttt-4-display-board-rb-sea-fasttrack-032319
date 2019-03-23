8a0e413ed6b2b39389768da056a211277bacf751
Katana Tran <tkatyk21@gmail.com>
2019-03-23 21:51:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board("#{board}")
-  puts " #{} | #{} | #{} "
+def display_board("board}")
+  puts " #{1} | #{2} | #{3} "
   11.times{print("-")}
   puts ""
-  puts " #{} | #{} | #{} "
+  puts " #{4} | #{5} | #{6} "
   11.times{print("-")}
   puts ""
-  puts " #{} | #{} | #{} "
+  puts " #{7} | #{8} | #{9} "
 end 
 
 
